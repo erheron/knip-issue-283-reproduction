8a89550b97176afd7545038ff93616f676236acb
Władysław Raczek <wsw.raczek@gmail.com>
2023-10-09 17:55:51 +0200

    Change knip

--- knip.ts
+++ knip.ts
@@ -3,30 +3,23 @@
 export default {
   workspaces: {
     ".": {
-      entry: ["src/App.tsx"],
+      entry: ["src/index.ts"],
       project: ["src/**"],
-      ignore: [
-        "**/__generated__",
-        "**/__graphql__",
-        "**/__devMocks__",
-        "codegen.ts",
-        "*.js,",
-        "*.e2e.tsx",
-      ],
+      ignore: [],
       ignoreDependencies: ["node_modules/**"],
     },
   },
   // see https://github.com/webpro/knip#reading-the-report
   include: ["exports", "types", "duplicateExports"],
   exclude: [
-    "files", // we do not care about empty files for now
-    "dependencies", // TODO MOBILE-3891 maybe enable this later
-    "devDependencies", // TODO MOBILE-3891 maybe enable this later
+    "files",
+    "dependencies",
+    "devDependencies",
     "binaries", // TS should handle this
     "unresolved", // TS should handle this
     "nsExports", // this is weaker than "exports", and "exports" is included
     "nsTypes", // this is weaker than "types", and "types" is included
-    "enumMembers", // TODO MOBILE-3891 maybe enable this later
-    "classMembers", // TODO MOBILE-3891 maybe enable this later
+    "enumMembers",
+    "classMembers",
   ],
 }; // satisfies KnipConfig;
